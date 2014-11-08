410ad1b78ebc994c26ba13277d5961f98ec32c3f
beastMode <nicholas.desouza@uoit.net>
2014-11-08 17:14:45 -0500

    created blockio.java: needs init_disk method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	    blockio Disk1 = new blockio();
+        String testFile = "Test";
+        byte[] dataTest;
+        dataTest = stringToByteASCII(testFile);
+        Disk1.putBlock(0, dataTest);
+    }
+
+    public static byte[] stringToByteASCII(String str) {
+        char[] buffer = str.toCharArray();
+        byte[] b = new byte[128];
+        for (int i = 0; i < b.length; i++) {
+            if (i < buffer.length) {
+                b[i] = (byte) buffer[i];
+            } else {
+                b[i] = (byte) 0;
+            }
+        }
+        return b;
     }
 }
+
+
--- src/com/company/blockio.java
+++ src/com/company/blockio.java
@@ -17,20 +17,46 @@ public class blockio {
     private static final int S_IRWXG = 00070;
 
 
-    private static int diskfd = -1;
+    private static FileDescriptor diskfd;
 
     private File fileName;
     private boolean bool = false;
     private RandomAccessFile disk;
 
     public blockio() {
+        diskfd = null;
         try{
             fileName = new File(DISKFILE);
+            bool = fileName.createNewFile();
             disk = new RandomAccessFile(fileName, "rw");
         } catch (IOException e) {
             System.err.println ("Unable to start disk");
             System.exit(1);
         }
+    }
+
+    private int init_disk(){
+        char garbage = '\0';
+        try {
+            diskfd = disk.getFD();
+        } catch (IOException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+
+        try {
+            disk.seek((long)(BLKSIZE*NUMBLKS));
+        } catch (IOException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+
+        try {
+            disk.write(garbage);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return 0;
     }
 
     private void seek(int blocknum) throws IOException {
@@ -42,34 +68,39 @@ public class blockio {
     }
 
 
-    public void getBlock(int blocknum, byte[] buffer) {
+    public int getBlock(int blocknum, byte[] buffer) {
         if (buffer.length != BLKSIZE) {
             throw new RuntimeException("getBlock: bad buffer size "
                     + buffer.length);
+        }
+        if (diskfd == null){
+            if (init_disk() != 0) return -1;
         }
         try {
             seek(blocknum);
             disk.read(buffer);
         } catch (IOException e) {
             System.err.println(e);
-            System.exit(1);
+            return -1;
         }
+        return 0;
     }
 
-    public void putBlock(int blocknum, byte[] buffer){
+    public int putBlock(int blocknum, byte[] buffer){
         if (buffer.length != BLKSIZE){
             throw new RuntimeException("putBlock: bad buffer size "
                     + buffer.length);
+        }
+        if (diskfd == null){
+            if (init_disk() != 0) return -1;
         }
         try {
             seek(blocknum);
             disk.write(buffer);
         } catch(IOException e) {
             System.err.println(e);
-            System.exit(1);
+            return -1;
         }
+        return 0;
     }
-
-
-
 }
