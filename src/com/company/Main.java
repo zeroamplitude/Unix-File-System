0fdb5c6364a3f9347879952a7ae5ffd71961db46
beastMode <nicholas.desouza@uoit.net>
2014-11-08 18:06:00 -0500

    getBlock test: Successful

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,12 @@ public class Main {
         byte[] dataTest;
         dataTest = stringToByteASCII(testFile);
         Disk1.putBlock(0, dataTest);
+
+        byte[] dataTest2 = new byte[128];
+        Disk1.getBlock(0, dataTest2);
+        String test2 = new String(dataTest2);
+        System.out.println(test2);
+
     }
 
     public static byte[] stringToByteASCII(String str) {
@@ -21,6 +27,17 @@ public class Main {
             }
         }
         return b;
+    }
+
+    public static String bytesToString(byte[] bytes) {
+        char[] buffer = new char[bytes.length >> 1];
+        for(int i = 0; i < buffer.length; i++) {
+             int bpos = i << 1;
+             char c = (char)(((bytes[bpos]&0x00FF)<<8)
+                     + (bytes[bpos+1]&0x00FF));
+             buffer[i] = c;
+        }
+        return new String(buffer);
     }
 }
 
