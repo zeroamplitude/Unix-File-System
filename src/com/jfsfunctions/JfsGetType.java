61c800c1a54a02762a34427325cf2b686b84a8fc
mysteryhobo <thepeterlittle@gmail.com>
2014-11-09 14:31:35 -0500

    Started get size and get type class cannot finish until internals are complete

--- src/com/jfsfunctions/JfsGetType.java
+++ src/com/jfsfunctions/JfsGetType.java
@@ -1,10 +1,39 @@
 package com.jfsfunctions;
 
+import java.util.StringTokenizer;
+
 /**
  * @author <<Fill in your name here>>
  */
 public abstract class JfsGetType implements JfsInterface {
     public int jfsGetType(String pathname) {
+
+         /*
+         * parse the pathname to determine if it is a correct pathname
+         */
+        int tokenCounter = 0;
+        String token;
+        String[] tokens = new String[64];
+        StringTokenizer tokenizer = new StringTokenizer(pathname, "\\");
+
+        while (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+            System.out.println(token);
+            tokenCounter ++;
+
+            if (token.length() > 5){
+                System.out.println("ERROR: INVALID PATHNAME");
+                return 0;
+            }else tokens[tokenCounter - 1] = token;
+        }
+
+        if (tokenCounter == 0){
+            System.out.println("ERROR: INVALID PATHNAME");
+            return 0;
+        }
+
+        String fileName = tokens[tokenCounter - 1];
+        System.out.println(fileName);
         return 0;
     }
 }
