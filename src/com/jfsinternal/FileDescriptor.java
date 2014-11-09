3f470b69ca16ee9d9ffe93714796fe6b3973ae2f
MKCY <milankornicer@gmail.com>
2014-11-09 14:45:34 -0500

    milan trying to git push

---
+++ src/com/jfsinternal/FileDescriptor.java
@@ -0,0 +1,8 @@
+package com.jfsinternal;
+
+/**
+ * Created by 100487498 on 09/11/2014.
+ */
+public class FileDescriptor {
+
+}
--- src/com/jfsinternal/INode.java
+++ src/com/jfsinternal/INode.java
@@ -6,39 +6,10 @@ package com.jfsinternal;
 public class INode {
 
     public final static int SIZE = 64;
-    int flags;
+    int type;
     int owner;
     int fileSize;
-    public int[] pointer;
-
-    /**
-     * Constructor class
-     */
-    public INode() {
-
-    }
-
-    int putINodeTable() {
-
-        return 0;
-    }
-
-    int getFilePointer(int iNumber, int filePtr) {
-    return 0;
-    }
-
-    int allocBlk2File(int iNumber, int allocBlkNo) {
-    return 0;
-    }
-
-    int parsePathName(String path, int numComponents) {
-    return 0;
-    }
-
-    int parseDirEntry(int componentNo, int component, int iNumber) {
-
-    return 0;
-    }
+    int[] directAccess = new int[11];
 
 
 }
