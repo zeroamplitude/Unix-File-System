ab235e3dda735c4805eab64e669a535a11bd4d83
beastMode <nicholas.desouza@uoit.net>
2014-11-26 21:23:15 -0500

    Fixing: Create

--- src/com/jfsfunctions/JfsCreate.java
+++ src/com/jfsfunctions/JfsCreate.java
@@ -1,14 +1,117 @@
 package com.jfsfunctions;
 
+import com.jfsinternal.BlockIO;
+import com.jfsinternal.SuperBlock;
+import sun.org.mozilla.javascript.ast.Block;
+
 /**
  * @author Nicholas De Souza
+ *
+ * TODO: Improve disk access by reading iNode Table to memory
  */
 public class JfsCreate extends JfsInterface {
+
+    private static final int NUMFILES = 64;
+    private static final int BLKSIZE = 128;
+    public static BlockIO disk = new BlockIO();
+
+    /**
+     * jfsCreate:
+     *      This method takes the pathname and type specified by
+     *      the user and checks to make sure the file does not
+     *      exist on file. If no file exist it creates a new iNode
+     *      in the iNodeTable.
+     *
+     * @param pathname
+     * @param type
+     * @return
+     *
+     * TODO: Merge all functions into one
+     */
     @Override
     public int jfsCreate(String pathname, int type) {
-        //hello
+        int error = 0;
+
+        error = searchITable(pathname);
+        if (error < 0) {
+            if (error == -2) {
+                System.out.println("File already exists on drive");
+            }
+            return -1;
+        }
+
+        error = getFreeINode();
+        if (error < 0) {
+            if (error == -2) {
+
+            }
+        }
+        return 0;
+
+        error = createINode(pathname);
+        if (error < 0) {
+            if (error == -2) {
+                System.out.println("No room on disk");
+            }
+            return -1;
+        }
+
+
+    }
+
+    public int searchITable(String pathname) {
+        byte[] buffer = new byte[BLKSIZE];
+        for(int i = 2; i < 8; i++) {
+            try {
+                disk.getBlock(i, buffer);
+            } catch (Exception e) {
+                System.out.println("Disk read error: "
+                        + e);
+            }
+            for(int j = 2; j < BLKSIZE; j += 16) {
+                String curname;
+                try {
+                    curname = new String(buffer, j, 6);
+                    if (curname == pathname) {
+                        return -2;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Name read error: "
+                            + e);
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public short getFreeINode() {
+        byte[] buffer = new byte[BLKSIZE];
+        short curInum = 0;
+        for(int i = 0; i < 8; i++) {
+            try {
+                disk.getBlock(i, buffer);
+            } catch (Exception e) {
+                System.out.println("Disk read error: "
+                        + e);
+                return -1;
+            }
+            for(int j = 0; j < BLKSIZE; j += 16) {
+                if(buffer[j] == (byte) 0) {
+                    return curInum;
+                }
+                curInum++;
+            }
+        }
+        return (short) -2;
+    }
+
+    public int createINode(String pathname) {
+        byte[] buffer = new byte[BLKSIZE];
         
 
         return 0;
     }
+
+
 }
--- src/com/jfsinternal/INode.java
+++ src/com/jfsinternal/INode.java
@@ -4,24 +4,31 @@ package com.jfsinternal;
  * Created by nicholas on 26/11/14.
  */
 public class INode implements JfsInternalConstants{
-    public short iNumber;
-    public short size;
-    public short Type;
-    public char[] name;
 
+    private short iNumber;
+    private byte status;
+    private short size;
+    private short Type;
+    private String name;
+    private short openCount;
+    private short cdate;
+    private short adate;
+    private short mdate;
     public short direct[] = new short[11];
     public short indirect[];
 
-    INode(short location) {
-
+    INode(short location, String name, short Type) {
+        this.status = 1;
         this.size = 0;
-        this.Type = 1;
+        this.name = name;
+        this.Type = Type;
+        this.openCount =0;
     }
 
-//    public INode(short size, short Type, short[] direct, short indirect)  {
-//        this.size = size;
-//        this.Type = Type;
-//
-//    }
+    public INode(short size, short Type, short[] direct, short indirect)  {
+        this.size = size;
+        this.Type = Type;
+
+    }
 
 }
