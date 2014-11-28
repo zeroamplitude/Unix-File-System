58cde7f7277674d119fd9b89b6a6fb22909131f0
beastMode <nicholas.desouza@uoit.net>
2014-11-27 20:21:15 -0500

    JfsInitialize will write superblock to disk

--- src/com/jfsfunctions/JfsInitialize.java
+++ src/com/jfsfunctions/JfsInitialize.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of JavaFileSystem (jfs).
+ *
+ *     jfs is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     jfs is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with jfs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2014. Nicholas De Souza, Arun Gireesan, Milan Kornicer, Peter Little. All rights reserved
+ */
+
 package com.jfsfunctions;
 
 import com.jfsinternal.*;
@@ -15,6 +34,7 @@ public class JfsInitialize extends JfsInterface {
     /**
      * @param erase
      * @return 0 if successful, -1 on failure
+     * TODO: Needs Work
      */
     @Override
     public int jfsInitialize(int erase) {
@@ -73,7 +93,7 @@ public class JfsInitialize extends JfsInterface {
         return 0;
     }
     private int clearInodeTable() {
-        byte[] clear = new byte[sb.BLKSIZE];
+        byte[] clear = new byte[JfsInternalConstants.BLKSIZE];
         try {
             for (int i = 1; i <= sb.iNodeTable; i++) {
                 disk.putBlock((i + 1), clear);
--- src/com/jfstest/JfsTest.java
+++ src/com/jfstest/JfsTest.java
@@ -1,8 +1,30 @@
+/*
+ * This file is part of JavaFileSystem (jfs).
+ *
+ *     jfs is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     jfs is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with jfs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2014. Nicholas De Souza, Arun Gireesan, Milan Kornicer, Peter Little. All rights reserved
+ */
+
 package com.jfstest;
 
-import com.jfsfunctions.*;
-import java.util.Scanner;
+import com.jfsfunctions.JfsCreate;
+import com.jfsfunctions.JfsInitialize;
+import com.jfsfunctions.JfsInterface;
+
 import javax.validation.constraints.Size;
+import java.util.Scanner;
 
 /**
  * JfsTest:
@@ -25,6 +47,7 @@ import javax.validation.constraints.Size;
  *
  *
  * @author Nicholas De Souza
+ * TODO: Fix null pointer exception after first call is executed
  */
 public class JfsTest {
 
